4584defa9b40cdf0d8a917379d2d66ffc5df955e
ヾ(≧∇≦*)ゝ <iluvmizuki@gmail.com>
2018-06-12 16:56:56 +0200

    Striker is kinda working

--- src/main.c
+++ src/main.c
@@ -25,104 +25,124 @@
 //#define BORDERX 150
 //#define BORDERY 80
 
-void print_striker(uint8_t SS,  uint8_t *striker_x, uint8_t striker_y){
-/*
-  *(striker_x) = (BORDERX/2-(SS/2));
-  *(striker_y) = (BORDERY-3);
-*/
 
 
-  gotoxy((striker_x)*, (striker_y)*);
-  for (int i = 0; i < SS; ++i)
+//void initVector(struct vector_t *v, int32_t x, int32_t y) {
+//    v->x = x << 14;
+//    v->y = y << 14;
+//};
+
+
+
+struct striker_t{
+    struct vector_t posi;
+    struct vector_t vel;
+};
+
+
+void init_striker(uint8_t BORDERX, uint8_t BORDERY, uint8_t SS, struct striker_t *ks){
+
+ initVector(&ks->posi, ( ( ( BORDERX/2 )- ( SS/2 ) )), (BORDERY-3));
+ initVector(&ks->vel, 0 << 14 , 0 << 14);
+
+
+}
+
+void print_striker(uint8_t SS, struct striker_t *ks ){
+
+  for ( uint8_t i = 0; i < SS; i++ )
   {
+    gotoxy( ( ( ( ks->posi.x ) >> 14 ) + i ) , ( ( ks->posi.y ) >> 14 )  );
     printf("%c",219);
+
   }
-  
-
-
 }
 
 
+void update_striker(struct striker_t *ks, uint8_t SS){
 
 
 
-void init_striker(uint8_t BORDERX, uint8_t BORDERY, uint8_t SS, uint8_t *striker_x, uint8_t striker_y){
-*striker_x = (BORDERX/2)-(SS/2);
-*striker_y = (BORDERY)-3;
+      for ( uint8_t i = 0; i < SS; i++ )
+  {
+    gotoxy( ( ( ( ks->posi.x ) >> 14 ) + i ) , ( ( ks->posi.y ) >> 14 )  );
+    printf("%c", 255);
 
-  print_striker(SS, *striker_x, striker_y);
+  }
 
-}
 
-void move_striker(uint8_t SS, uint8_t *striker_x, uint8_t striker_y){
-  uint8_t movement;
 
-    if (readJoystick( )==3){
-    movement = -3;
+
+    if (readJoystick()== 4){
+        ks->vel.x = -10 << 14;
+
+
+     for ( uint8_t i = 0; i < SS; i++ )
+  {
+    gotoxy( ( ( ( ks->posi.x ) >> 14 ) + i ) , ( ( ks->posi.y ) >> 14 )  );
+    printf("%c", 255);
+
+  }
+
+
     }
-    else if( readJoystick == 4){
-      movement = 3;
+    else if (readJoystick()== 8){
+
+
+        ks->vel.x = 10 << 14;
+
+     for ( uint8_t i = 0; i < SS; i++ )
+  {
+    gotoxy( ( ( ( ks->posi.x ) >> 14 ) + i ) , ( ( ks->posi.y ) >> 14 )  );
+    printf("%c", 255);
+
   }
 
 
 
-  *(striker_x)= *(striker_x) + movement;
+    }
+    else if(readJoystick()== 0){
+
+        ks->vel.x = 0 << 14;
+
+
+    }
+
+
+   ks->posi.x = ks->posi.x + FIX14_MULT(ks->vel.x,K);
+    ks->posi.y = ks->posi.y + FIX14_MULT(ks->vel.y,K);
+
+
+    print_striker(SS, ks );//update position
 
 }
+
 
 int main(void)
 {
 
-
-
-
     init_usb_uart( 115200 );
     uint8_t ss = 6;
-    uint8_t BORDERX = 152;
+    uint8_t BORDERX = 150;
     uint8_t BORDERY = 80;
-
     init_joystick();
 
- 
+
     clrscr();
     gotoxy(1,1);
 
-init_joystick;
-init_striker;
+init_joystick();
+//init_striker(BORDERX, BORDERY, ss, *(striker_x), (striker_y));
+
+ struct striker_t strike;
+
+init_striker(BORDERX, BORDERY, ss, &strike);
 
 
   while(1)  {
+update_striker(&strike, ss);
 
-
-move_striker(SS, *striker_x, striker_y);
-
-//        if((b.posi.x <= (2 << 14)) || (b.posi.x >= ((BORDERX) << 14 )))
-//    {
-//    b.vel.x = -b.vel.x;
-//    gotoxy(b.posi.x >> 14,b.posi.y >> 14);
-//    printf("%c",179);
-//    }
-//    else if ((b.posi.y >= ((BORDERY)<< 14)) || (b.posi.y <= (2<<14)))
-//        {
-//        b.vel.y = -b.vel.y;
-//        gotoxy(b.posi.x >> 14,b.posi.y >> 14);
-//        printf("%c",196);
-//        }
-//
-//
-//    updatepos(&b);
-//if ((b.posi.x <= (BORDERX/2+6 << 14)) && (b.posi.x >= ((BORDERX/2-6) << 14 ))
-//    && (b.posi.y <= (BORDERY/2+2 << 14)) && (b.posi.y >= ((BORDERY/2-2) << 14 ))) {}
-// //  counter(borderX/2-5,borderY/2-1,borderX/2+5,borderY/2+1);
-//
-//    else {
-//
-//
-//    gotoxy(b.posi.x >> 14,b.posi.y >> 14);
-//    printf("o");
-//
-//    gotoxy(b.posi.x >> 14,b.posi.y >> 14);
-//    printf(" ");
     }
-  }
 
+
+}
