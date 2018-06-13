fbb69ea9c660eaa73fa718f229faa8341833a0f0
ヾ(≧∇≦*)ゝ <krisdanoda@users.noreply.github.com>
2018-06-13 03:52:19 +0200

    not hating c but not loving it

--- src/main.c
+++ src/main.c
@@ -37,6 +37,8 @@
 struct striker_t{
     struct vector_t posi;
     struct vector_t vel;
+    int8_t state;
+    int32_t a;
 };
 
 
@@ -44,18 +46,29 @@ void init_striker(uint8_t BORDERX, uint8_t BORDERY, uint8_t SS, struct striker_t
 
  initVector(&ks->posi, ( ( ( BORDERX/2 )- ( SS/2 ) )), (BORDERY-3));
  initVector(&ks->vel, 0 << 14 , 0 << 14);
+ int8_t *state =0;
 
+ print_striker(SS, ks );
 
 }
 
 void print_striker(uint8_t SS, struct striker_t *ks ){
 
-  for ( uint8_t i = 0; i < SS; i++ )
-  {
-    gotoxy( ( ( ( ks->posi.x ) >> 14 ) + i ) , ( ( ks->posi.y ) >> 14 )  );
-    printf("%c",219);
+//  for ( uint8_t i = 0; i < SS; i++ )
+//  {
+//    gotoxy( ( ( ( ks->posi.x ) >> 14 ) + i - SS/2 ) , ( ( ks->posi.y ) >> 14 )  );
+//    printf("%c",219);
+//
+//  }
+if (ks->state == 1){
+  gotoxy(  ( ( ks->posi.x  - (SS/2 << 14) )>> 14  ) , ( ( ks->posi.y ) >> 14 )  );
+printf(" (^>_<^) ");
+}
+ else{
+         gotoxy(  ( ( ks->posi.x  - (SS/2 << 14) )>> 14  ) , ( ( ks->posi.y ) >> 14 )  );
+printf(" (^._.^) ");
+ }
 
-  }
 }
 
 
@@ -88,7 +101,7 @@ int32_t old_x = ks->posi.x;
 //      printf("d");
 //    }
 
-gotoxy( ( ( ( old_x ) >> 14 ) + SS ) , ( ( ks->posi.y ) >> 14 ) );
+gotoxy( ( ( ( old_x ) >> 14 ) + SS/2 ) , ( ( ks->posi.y ) >> 14 ) );
     printf(" ");
 
     print_striker(SS, ks );//update position
@@ -114,14 +127,80 @@ gotoxy( ( ( ( old_x ) >> 14 ) + SS ) , ( ( ks->posi.y ) >> 14 ) );
         ks->vel.x = 0 << 14;
 
     }
+
+    if (ks->a > 700){
+    ks -> state = 0;
+    print_striker(SS, ks );
+    }
+    (ks ->a)= (ks ->a)+1;
 }
-int8_t cc = 0;
+
+striker_bounce(struct striker_t *ks, uint8_t SS, struct ball_t *b){
+if  ( ( (ks->posi.y) >> 14 ==  (b->posi.y) >> 14 ) && ( (  b->posi.x >> 14 ) >= ( ( ks->posi.x - ((SS/2) << 14)) >> 14 ) ) && ( ( ( b->posi.x >> 14 )  ) <= ( ( ( ks->posi.x + ((SS/2) << 14)) >> 14 ) ) ) ) {
+int32_t mini = 10000;
+int32_t g_angle;
+ //  b->vel.y = -(b->vel.y);
+
+uint32_t dif = abs(b-> posi.x - ks -> posi.x)*2 / SS;
+
+
+for (uint32_t i = 0 ; i <= 128; i++ )
+    {
+int32_t it = 10000 * (uint32_t)(expand(SIN[i]) & 0xFFFF) >> 16;
+
+        if (mini > ((abs(dif - it)*10000))<<14){
+        mini = ((abs(dif - it)*10000)<<14);
+        g_angle = i;
+        }
+    }
+
+
+//rotate(&b.vel, g_angle);
+//b -> vel.y = - (b -> vel.y);
+//rotate(*b.vel, -g_angle);
+gotoxy(20,20);
+
+
+if ( (b-> posi.x - ks -> posi.x) <= 0){
+
+
+}
+else if( (b-> posi.x - ks -> posi.x) >= 0){
+
+g_angle = - g_angle;
+}
+
+printf("%d \n", g_angle);
+printf("%d \i",( b-> posi.x - ks -> posi.x) >> 14);
+
+
+
+int32_t x1 = b->vel.x;
+ b->vel.x = FIX14_MULT(b->vel.x,Cos(g_angle))-FIX14_MULT(b->vel.y,Sin(g_angle));
+    b->vel.y = FIX14_MULT(x1,Sin(g_angle))+FIX14_MULT(b->vel.y,Cos(g_angle));
+ b->vel.y =  -(b->vel.y);
+
+x1 = b->vel.x;
+    b->vel.x = FIX14_MULT(b->vel.x,Cos(-g_angle))-FIX14_MULT(b->vel.y,Sin(-g_angle));
+    b->vel.y = FIX14_MULT(x1,Sin(-g_angle))+FIX14_MULT(b->vel.y,Cos(-g_angle));
+
+     b->posi.y =  b->posi.y - (2 << 14);
+
+    ks->state =1;
+    ks->a =0;
+
+    print_striker(SS, ks );
+}
+}
+
+//( (ks->posi.y) >> 14 ==  (b->posi.y) >> 14 )
+
 
 int main(void)
 {
     init_usb_uart( 115200 );
       printf("%c[?25l",ESC);
-    uint8_t ss = 6;
+    uint8_t ss = 7;
     uint8_t BORDERX = 150;
     uint8_t BORDERY = 80;
     init_joystick();
@@ -129,18 +208,30 @@ int main(void)
     clrscr();
     gotoxy(2,2);
 
-    printf("~(=^%c%c^)", 250, 250);
+
 
 init_joystick();
 //init_striker(BORDERX, BORDERY, ss, *(striker_x), (striker_y));
 
  struct striker_t strike;
+struct ball_t b;
 
 init_striker(BORDERX, BORDERY, ss, &strike);
+ initVector(&b.posi, BORDERX/2,20);
+ initVector(&b.vel, 0,1);
+
+
 
   while(1)  {
 update_striker(&strike, ss);
-gotoxy(20,20);
-printf("%c", cc);
+
+updatepos(&b);
+striker_bounce(&strike, ss, &b);
+   gotoxy(b.posi.x >> 14,b.posi.y >> 14);
+    printf("%c", 220 );
+
+    gotoxy(b.posi.x >> 14,b.posi.y >> 14);
+    printf(" ");
     }
+
 }
