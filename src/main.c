63fc5d232e38ecc38e640ec2919c5e243e46e2d1
S143277 <danjal_berg@msn.com>
2018-06-14 09:29:58 +0200

    some comments

--- src/main.c
+++ src/main.c
@@ -26,8 +26,8 @@
 #include "block_control.h"
 
 
-#define BORDERX 150
-#define BORDERY 60
+#define BORDERX 150                                             //define the size of the window
+#define BORDERY 60                                              //define the size of the window
 
 
 
@@ -36,86 +36,84 @@
 int main(void)
 {
 
-    init_usb_uart( 115200 );
+    init_usb_uart( 115200 );                                    //set speed of microprocessor
 
-    clrscr();
-    gotoxy(1, 1);
-    printf("%c[?25l", ESC);
-    counter(1, 1, BORDERX, BORDERY);
-    init_joystick();
-    init_RGB();
-    init_spi_lcd();
-    init_menu();
-    init_disp_score();
+    clrscr();                                                   // clear screen
+    gotoxy(1, 1);                                               // go to the first pixel in command prompt
+    printf("%c[?25l", ESC);                                     // remove visible cursor
+    counter(1, 1, BORDERX, BORDERY);                            // create the window with border size
+    init_joystick();                                            // initialize joystick
+    init_RGB();                                                 // initialize RGB
+    init_spi_lcd();                                             // initialize LCD display
+    init_menu();                                                // initialize start menu
+    init_disp_score();                                          // initialize display score
 
     // Variables in main
-    uint8_t ss = 9;
-    uint8_t in_game = 0;
+    uint8_t ss = 9;                                             // size of striker
+    uint8_t in_game = 0;                                        // if 0 menu , 1 initialize game , 2 in game.
 
-    uint8_t menu_counter = 1;       // used in menu
-    uint8_t old_read = 0;           // used in menu
-    uint8_t life_count = 3;         // to show with RGB
-    uint32_t score_counter = 0;     // to print on LCD
-    uint8_t level_counter = 1;
-    uint16_t speed_multi = 12;       // Used to adjust speed
+    uint8_t menu_counter = 1;                                   // used in menu
+    uint8_t old_read = 0;                                       // used in menu
+    uint8_t life_count = 3;                                     // to show with RGB
+    uint32_t score_counter = 0;                                 // to print on LCD
+    uint8_t level_counter = 1;                                  // keep track of level
+    uint16_t speed_multi = 12;                                  // Used to adjust speed
 
 
     // Init balls
-    struct ball_t b;
-    struct striker_t strike;
+    struct ball_t b;                                            // initialize struct of the ball
+    struct striker_t strike;                                    // initialize struct of the striker
 
     // Init blocks
-	struct blockpos block[27];
+    struct blockpos block[27];                                  // initialize boxes in game
 
     while (1)
     {
-        // Display life_count on RGB
-        set_RGB(life_count);
-        // Display score_counter on LCD
-        write_score(score_counter);
-        // Display score_counter on LCD
-        write_level(level_counter);
 
-        // Navigate menu with joystick
-        uint8_t read = readJoystick();
+        set_RGB(life_count);                                    // Display life_count on RGB
+        write_score(score_counter);                             // Display score_counter on LCD
+        write_level(level_counter);                             // Display score_counter on LCD
+
+
+        uint8_t read = readJoystick();                          // Navigate menu with joystick
         control_menu(read, &menu_counter, &old_read, &in_game);
 
         if ((in_game) == 1) {
             uint8_t text_line_1 = BORDERY / 5;
-            clear_line(text_line_1);   // Delete old text
+            clear_line(text_line_1);                            // Delete old text
             clear_line(text_line_1 + 2);
             clear_line(text_line_1 + 4);
 
-        in_game++;
+            in_game++;
 
-        init_blocks(block, 1); // create blocks and input level
-        init_striker(BORDERX, BORDERY, ss, &strike);
-        initVector(&b.posi, 20, 45);
-        initVector(&b.vel, 2, 1);
+            init_blocks(block, 1);                              // create blocks and input level
+            init_striker(BORDERX, BORDERY, ss, &strike);
+            initVector(&b.posi, 20, 45);
+            initVector(&b.vel, 2, 1);
+        }
+
+
+        if (in_game == 2) {
+
+
+            update_striker(&strike, ss);
+            striker_bounce(&strike, ss, &b);
+
+            gotoxy(b.posi.x >> 14, b.posi.y >> 14);
+            printf(" ");
+
+            border_control(&b);
+            score_counter = block_control(&b, block, score_counter);
+            // Check for bounches and hits
+
+            updatepos(&b, speed_multi);
+
+            // Print new ball
+            gotoxy(b.posi.x >> 14, b.posi.y >> 14);
+            printf("%c", 254);
+
+        }
     }
-
-
-        if (in_game==2){
-
-
-        update_striker(&strike, ss);
-        striker_bounce(&strike, ss, &b);
-
-        gotoxy(b.posi.x >> 14, b.posi.y >> 14);
-        printf(" ");
-
-        border_control(&b);
-        score_counter = block_control(&b, block, score_counter);
-        // Check for bounches and hits
-
-		updatepos(&b, speed_multi);
-
-        // Print new ball
-		gotoxy(b.posi.x >> 14, b.posi.y >> 14);
-		printf("%c", 254);
-
-        }
-        }
 
 }
 
